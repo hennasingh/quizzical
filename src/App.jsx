0bb5785402a582dc3d189a4046f413c71c636210
Henna Singh <hennasingh04@gmail.com>
2023-05-29 17:42:56 +0100

    initial commit with update in App.js page with button to start quiz

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='quiz-title'>Quizzical</h1>
+     <p>A fun quiz from multiple categories. 
+      <br/>Let's see how much time you take to solve.</p>
+      <button className='start-quiz'>Start Quiz</button>
+    </>
+  )
+}
+
+export default App
