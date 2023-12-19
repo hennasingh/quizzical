5e9587a6a6901621dfeb618d21a67247c357dbd5
Henna Singh <hennasingh04@gmail.com>
2023-12-19 17:59:47 +0000

    separated combine options section from the jsx code to a separate function called after fetch

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -4,43 +4,82 @@ import { decode } from 'html-entities';
 export default function Questions() {
 
     const[questions, setAllQuestions] = React.useState('')
+    const [selectedAnswers, setSelectedAnswers] = React.useState({})
 
     React.useEffect(() => {
         fetch("https://opentdb.com/api.php?amount=05&type=multiple")
         .then((res) => res.json())
-        .then((data)=> setAllQuestions(data.results))
+        .then((data)=> {
+            setAllQuestions(combineQuestions(data.results))
+            console.log(data.results)})
     },[])
 
-    const combinedOptions = (ques) => {
-        let options = []
-        for(let i = 0; i < ques.incorrect_answers.length; i++){
-            options.push(ques.incorrect_answers[i])
-        }
-       const index =  Math.ceil(Math.random() * options.length)
-       options.splice(index, 0, ques.correct_answer)
-       return options
+    function combineQuestions(questions) {
+       const answers =  questions.map((ques, quesId) => {
+            let options = []
+            for(let i = 0; i < ques.incorrect_answers.length; i++){
+                options.push(ques.incorrect_answers[i])
+            }
+           const answerId =  Math.ceil(Math.random() * options.length)
+           options.splice(answerId, 0, ques.correct_answer)
+            return {
+                'id': quesId ,
+                'question': ques.question,
+                options,
+                'answers': {quesId, answerId}
+            }
+        })
+        console.log(answers)
+        return answers
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(selectedAnswers)
+
+    }
+
+    function handleChange(quesId, optionId) {
+        setSelectedAnswers(prevAnswer => {
+            return {
+                ...prevAnswer,
+                [quesId]: optionId
+            }
+        })
     }
 
     if (questions) {
         return(
             <div className='container'>
-                 <form>
+                 <form onSubmit={handleSubmit}>
                     <div className='questions'>                  
                         {
-                            questions.map((ques, idx) => (
-                                <div key={idx}>
+                            questions.map((ques, quesId) => (
+                                <div key={quesId}>
                                     <p>{ decode(ques.question) }</p>
                                     <div className="options">
                                     
-                                        {
-                                                combinedOptions(ques).map((label, id) => (    
-                                                <div key={id}>                       
-                                                    <input type="radio" id={`question_${idx}_option${id}`} name={`question${idx}`}/>
-                                                    <label className="radio-button" htmlFor={`question_${idx}_option${id}`}>{decode(label)}</label>
+                                        {                                           
+
+                                            ques.options.map((label, optionId) => (    
+                                                <div key={optionId}>                      
+                                                    <input 
+                                                        type="radio" 
+                                                        id={`ques_${quesId}_option${optionId}`} 
+                                                        name={`ques_${quesId}`}
+                                                        value={optionId}
+                                                        checked ={selectedAnswers[quesId] === optionId}
+                                                        onChange = {() => handleChange(quesId, optionId)}
+                                                    />
+                                                    <label
+                                                        className="radio-button" 
+                                                        htmlFor={`ques_${quesId}_option${optionId}`}
+                                                    > 
+                                                    {decode(label)}
+                                                    </label>
                                                 </div>
                                             ))
-                                        }
-                                        
+                                        }                                       
                                     </div>
                                     <hr />
                                 </div>
